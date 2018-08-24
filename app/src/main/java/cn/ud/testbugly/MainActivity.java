3205ac4c7bcd875d4cc5f5b9e87af94bf2f99359
lynch.liu <lynch.liu@utstarcom.cn>
2018-08-24 10:14:11 +0800

    1第二个分支

--- app/src/main/java/cn/ud/testbugly/MainActivity.java
+++ app/src/main/java/cn/ud/testbugly/MainActivity.java
@@ -2,10 +2,8 @@ package cn.ud.testbugly;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 
 import com.tencent.bugly.Bugly;
-import com.tencent.bugly.crashreport.CrashReport;
 
 /**
  * @author liu
@@ -19,13 +17,13 @@ public class MainActivity extends AppCompatActivity {
 //        CrashReport.initCrashReport(getApplicationContext(), "d3c2795c01", true);
 
         Bugly.init(getApplicationContext(), "d3c2795c01", false);
-        findViewById(R.id.bt_test).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                CrashReport.testJavaCrash();
-
-            }
-        });
+//        findViewById(R.id.bt_test).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                CrashReport.testJavaCrash();
+//
+//            }
+//        });
 
 
     }
